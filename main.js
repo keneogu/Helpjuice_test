1b961c1fbb601fb49191510a48d9fb8a65bb8242
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-01-19 12:28:10 +0100

    add the desktop view

---
+++ main.js
@@ -0,0 +1,20 @@
+let select = document.getElementById("select");
+let list = document.getElementById("list");
+
+let selected = document.getElementById("selected");
+let listed = document.getElementById("listed");
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('nav-links')[0]
+
+select.onclick = function(){
+	list.classList.toggle("open")
+}
+
+selected.onclick = function(){
+	listed.classList.toggle("open")
+}
+
+toggleButton.onclick= function() {
+	navbarLinks.classList.toggle('active')
+}
